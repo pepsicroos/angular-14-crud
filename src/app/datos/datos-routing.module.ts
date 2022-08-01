1d69b582eb1699a551fa2f3fbe176c27288b9788
Rafael Villagomez Vega <38387069+pepsicroos@users.noreply.github.com>
2022-08-01 09:56:13 -0500

    Add files via upload

---
+++ src/app/datos/datos-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+const routes: Routes = [
+  {
+    path: 'datos/home',
+    component: HomeComponent,
+  },
+  {
+    path: 'datos/create',
+    component: CreateComponent,
+  },
+  {
+    path:'datos/edit/:id',
+    component: EditComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class DatosRoutingModule { }
